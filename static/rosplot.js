61f7837d524aa0116f7485f4a8c8f3e4a93a2f35
toast <ut.ieee.ras@gmail.com>
2013-09-29 23:48:55 -0500

    Restructured animation code and added basic chart outline

--- static/rosplot.js
+++ static/rosplot.js
@@ -10,16 +10,10 @@ var ROSPlot = (function() {
 
   var mjpeg = 'http://' + window.location.hostname + ':9002/stream';
 
-  var animate = (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function(callback) {
-      setInterval(callback, 100);
-    }
-  );
+
+  var time = function() {
+    return new Date().getTime();
+  };
 
 
   // Possible commands
@@ -49,7 +43,68 @@ var ROSPlot = (function() {
 
     plot: {
       help: 'plots params against time',
-      cmd: function() {}
+
+      cmd: function(input) {
+        if (input.length <= 1) return;
+
+        var topics = [];
+
+        for (var i = 1; i < input.length; i++) {
+          topics[i-1] = {
+            topic: input[i],
+            color: '#ae3030',
+            times: [],
+            values: []
+          };
+        }
+
+        var buffer = 5;
+
+        if (topics.length > 1) {
+          this.title(topics[0].topic + '...');
+        } else {
+          this.title(topics[0].topic);
+        }
+
+        var draw = function(ctx) {
+          var off = 3;
+          var min_y = off;
+          var max_y = this.height - off;
+          var min_x = off;
+          var max_x = this.width - off*9;
+
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = '#555';
+          ctx.beginPath();
+          ctx.moveTo(max_x, min_y-off);
+          ctx.lineTo(max_x, max_y+off);
+          ctx.moveTo(min_x-off, max_y);
+          ctx.lineTo(max_x+off, max_y);
+
+          var y = ~~((max_y-min_y)/2 + min_y);
+          ctx.moveTo(max_x-off, min_y);
+          ctx.lineTo(max_x+off, min_y);
+          ctx.moveTo(max_x-off, y);
+          ctx.lineTo(max_x+off, y);
+
+          for (var i = 0; i < buffer; i++) {
+            var x = ~~((max_x-min_x) * (i/buffer) + min_x);
+            ctx.moveTo(x, max_y-off);
+            ctx.lineTo(x, max_y+off);
+          }
+
+          ctx.stroke();
+
+          ctx.fillStyle = '#f0f0f0';
+          ctx.font = '9px monospace';
+          ctx.fillText(0+'', max_x+2*off, max_y+off);
+          ctx.fillText(0.02 + '', max_x+2*off, y+off);
+          ctx.fillText(2.74839+'', max_x+2*off, min_y+off);
+        }
+
+        this.animate(draw);
+        this.plot(draw);
+      }
     },
 
     plot2: {
@@ -63,44 +118,27 @@ var ROSPlot = (function() {
       cmd: function(input) {
         if (input.length <= 1) return;
         var topic = input[1];
-
         var image = new Image();
-        var ctx = this.context;
-        var width;
-        var height;
 
         this.title(topic);
 
         this.plot(function(ctx) {
-          width = this.width;
-          height = this.height;
-
           var src = mjpeg;
           src += '?topic=' + topic;
-          src += '?width=' + width;
-          src += '?height=' + height;
+          src += '?width=' + this.width;
+          src += '?height=' + this.height;
           image.src = src;
         });
 
-        var draw = function() {
-          if (!ctx) return;
-
+        this.animate(function(ctx) {
           if (!image || image.width * image.height <= 0) {
             ctx.fillStyle = '#f0f0f0';
             ctx.font = '12px monospace';
-            ctx.fillText('Stream Error!!', 12, 12);
+            ctx.fillText('No Stream!', 12, 12);
           } else {
-            ctx.drawImage(image, 0, 0, width, height);
+            ctx.drawImage(image, 0, 0, this.width, this.height);
           }
-
-          animate(draw);
-        }
-
-        this.defered.push(function() {
-          ctx = null;
         });
-
-        draw();
       }
     }
   }
@@ -147,15 +185,50 @@ var ROSPlot = (function() {
 
   ROSPlot.prototype.clean = function() {
     for (var i=0; i < this.defered.length; i++) {
-      this.defered[i]();
+      this._defd = this.defered[i];
+      this._defd();
+      delete this._defd;
     }
 
     this.defered = [];
+
+    delete this._plot;
+    delete this._cmd;
+    delete this._animate;
   }
 
   ROSPlot.prototype.plot = function(plot) {
     this.clean();
     this._plot = plot;
+  }
+
+  ROSPlot.prototype.animate = function(plot) {
+    this._animate = plot;
+    var self = this;
+
+    var requestAnim = (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(callback) {
+        setInterval(callback, 100);
+      }
+    );
+
+    var draw = function() {
+      if (self._animate) {
+        // Clear the canvas
+        self.canvas.width = self.canvas.width;
+
+        // Call ourselves in through the closure
+        self._animate(self.context);
+        requestAnim(draw);
+      }
+    }
+
+    requestAnim(draw);
   }
 
   return ROSPlot;
